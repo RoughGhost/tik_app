ad4825d0b1a57eb15ab4c3b0c27068135769f0f8
RoughGhost <stephendlouis6@gmail.com>
2022-07-11 16:36:05 +0100

    first commit

---
+++ components/Discover.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { topics } from '../utils/constants'
+
+
+const Discover = () => {
+  return (
+    <div>
+        <h1>Discover</h1>
+    </div>
+
+  )
+}
+
+export default Discover
