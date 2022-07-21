8c002e4945adc871e9fa09bebff6847168dd1c05
RoughGhost <stephendlouis6@gmail.com>
2022-07-21 18:47:16 +0100

    first step commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,11 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
+import { createOrGetuser } from "../utils";
+import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
+  const { userProfile, addUser, removeUser } = useAuthStore();
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -21,6 +24,47 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10 ">
+            <Link href="/upload">
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {``}
+                <span className="hidden md:block ">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    src={userProfile.image}
+                    alt="profile photo"
+                    className="rounded-full cursor-pointer"
+                    layout="responsive"
+                    width={40}
+                    height={40}
+                  />
+                </>
+              </Link>
+            )}
+            <button
+              type="button"
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={35} className="px-2" />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetuser(response, addUser)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.capitalxtra.com"],
+    domains: ["www.capitalxtra.com", "lh3.googleusercontent.com"],
   },
 };
 
