19e219642f039fd31e47912d50d73a89d4f8685a
RoughGhost <stephendlouis6@gmail.com>
2022-07-22 13:26:00 +0100

    updated slug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,35 @@
-import type { NextPage } from 'next'
-import axios from "axios"
-import {Video} from "../types";
-import {VideoCard, NoResult} from "../components"
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import { VideoCard, NoResult } from "../components";
+import { BASE_URL } from "../utils";
 
 interface IProps {
-  videos:  Video[]
+  videos: Video[];
 }
 
-const Home = ({videos}: IProps) => {
+const Home = ({ videos }: IProps) => {
   return (
-    <div className="flex flex-col gap-10 videos h-full" >
-     {videos.length ? (
-       videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id} />
-       ))
-     ): (
-       <NoResult text={'No Videos'}  />
-     )}
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResult text={"No Videos"} />
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export const getServerSideProps = async () => {
-  const {data} = await axios.get(`http://localhost:3000/api/post`)
+  const { data } = await axios.get(`${BASE_URL}/api/post`);
 
   // console.log(response.data.name)
 
   return {
     props: {
-      videos: data
-    }
-  }
-}
+      videos: data,
+    },
+  };
+};
 
-export default Home
+export default Home;
